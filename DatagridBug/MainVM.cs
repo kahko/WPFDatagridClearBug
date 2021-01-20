a067d253d426955a1460cdc15b44941c905f10e4
Mikko Kahkonen <Mikko.Kahkonen@phillips-medisize.com>
2021-01-20 15:29:37 +0200

    Add project files.

---
+++ DatagridBug/MainVM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace DatagridBugNETFramework
+{
+    public class MainVM
+    {
+        public ObservableCollection<A> Collection { get; } = new ObservableCollection<A>();
+
+        private ICommand _clearCommand;
+        public ICommand ClearCommand
+        {
+            get
+            {
+                if (_clearCommand is null)
+                    _clearCommand = new RelayCommand(_ => Collection.Clear(), _ => Collection.Any());
+                return _clearCommand;
+            }
+        }
+    }
+
+    public class A
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ DatagridBugNETCore/MainVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace DatagridBugNETCore
+{
+    public class MainVM
+    {
+        public ObservableCollection<A> Collection { get; } = new ObservableCollection<A>();
+
+        private ICommand _clearCommand;
+        public ICommand ClearCommand
+        {
+            get
+            {
+                if (_clearCommand is null)
+                    _clearCommand = new RelayCommand(_ => Collection.Clear(), _ => Collection.Any());
+                return _clearCommand;
+            }
+        }
+    }
+
+    public class A
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
